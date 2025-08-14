6b771efebd2eb695560428fef5808eb76040f476
MikhaylovVladislav <79971836+MikhaylovVladislav@users.noreply.github.com>
2025-08-14 20:07:48 +0500

    Empty commit

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,9 @@
 import express from 'express'
 import { getProductsRoutes } from './routes/products'
 import { getHomeRoutes } from './routes/home'
-import { inputValidateMiddleware } from './middleware/input-validation-middleware'
 
 export const app = express()
-app.use(inputValidateMiddleware)
+
 app.use(express.json())
 
 app.use('/products', getProductsRoutes())
